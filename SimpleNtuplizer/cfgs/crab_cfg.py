93a85af905b0ade95f78e541809995cd90baa816
Thomas Klijnsma <klthomas@student.ethz.ch>
2016-05-19 17:03:17 +0200

    only different filename

--- SimpleNtuplizer/cfgs/crab_cfg.py
+++ SimpleNtuplizer/cfgs/crab_cfg.py
@@ -1,7 +1,8 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName              = 'TK_Ntuplizer_A2'
+#config.General.requestName              = 'TKNtup_1805_Photon'
+config.General.requestName              = 'TKNtup_1805_Electron'
 config.General.workArea                 = './'
 config.General.transferOutputs          = True
 config.General.transferLogs             = True
@@ -10,7 +11,8 @@ config.JobType.pluginName               = 'Analysis'
 config.JobType.psetName                 = 'test1_cfg.py'
 config.JobType.allowUndistributedCMSSW  = True
 
-config.Data.inputDataset                = '/DoublePhoton_FlatPt-5To300/RunIISpring16DR80-PUFlat0to50_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM'
+#config.Data.inputDataset                = '/DoublePhoton_FlatPt-5To300/RunIISpring16DR80-PUFlat0to50_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM'
+config.Data.inputDataset                = '/DoubleElectron_FlatPt-1To300/RunIISpring16DR80-PUFlat0to50_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM'
 config.Data.inputDBS                    = 'global'
 config.Data.splitting                   = 'FileBased'
 config.Data.unitsPerJob                 = 1
